a99aea861c4962670d196a189be50b66f27bc489
Sylvester Ouma <Sylvester.Ouma@jubileekenya.com>
2023-01-29 08:36:42 +0300

    adding iport React line

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
 import Tutor from './components/Tutor';
 import ParentTutor from './components/ParentTutor';
